6ac04ee9f67aaf4a0fb78b823510b691e3265c65
walkerwoid <ghost2121warrior@mail.ru>
2023-01-03 00:24:18 +0300

    added vue

--- script/script.js
+++ script/script.js
@@ -1,267 +1,108 @@
 const API = 'https://raw.githubusercontent.com/WalkerWoid/online-store-api/main'
 
-class ProductItem {
-    constructor(product, imgSrc = 'https://imgholder.ru/600x300') {
-        this.id = product.id
-        this.title = product.title
-        this.price = product.price
-        this.imgSrc = imgSrc
-        this.count = 0
-    }
-    render() {
-        return `<div class="product-item" data-id="${this.id}"> 
-                    <h3>${this.title}</h3>
-                    <img class="img-src" src="${this.imgSrc}"  alt="${this.title}">
-                    <p>${this.price} рублей</p>
-                    <buttton class="buy-btn purp-btn" data-id="${this.id}">Купить</buttton>
-                </div>`
-    }
-}
-class Catalog {
-    constructor(catalogSelector = '.products__container',
-                allPriceSelector = '.allPrice-btn',
-                addButtonSelector = 'buy-btn') {
-        this.catalogSelector = catalogSelector
-        this.productsFetch = []
-        this.products = []
-        this.allPriceSelector = allPriceSelector
-        this.startedPrice = 0
-        this.addButtonSelector = addButtonSelector
-        this.cart = cart
-        this.filteredItems = []
+const app = new Vue({
+    el: '#app',
+    data: {
+        products: [],
+        cartProducts: [],
+        filteredItems: [],
+        imgSrc: 'https://imgholder.ru/600x300',
+        cartVisibility: false,
+        isCartEmpty: false,
+        allPriceCart: 0,
+        inputText: ''
+    },
+    methods: {
+        fetchProducts(url) {
+            return fetch(url)
+                .then(data => data.json())
+                .catch(error => console.log(error))
+        },
+        addToCart(product) {
+            console.clear()
+            console.log(`добавляем в корзину товар`)
 
-        this.#init()
-    }
+            const isItemExist = this.isCartItemExist(product.id)
 
-    #init() {
-        this.#fetchProducts()
-            .then(() => {
-                this.#renderPage()
-            })
-        this.#eventsHandler()
-        document.querySelector('.filter-catalog').addEventListener('input', (event) => this.#filter(event))
-    }
-    #eventsHandler() {
-        const allPriceButton = document.querySelector(`${this.allPriceSelector}`)
-
-        const catalog = document.querySelector(`${this.catalogSelector}`)
-        catalog.addEventListener('click', (event) => this.#addToCart(event))
-    }
-    #countPrice(allPriceButton) {
-        this.startedPrice = 0
-        
-        for (const product of this.products) {
-            this.startedPrice += product.price
-        }
-
-        allPriceButton.innerText = this.startedPrice
-    }
-    #fetchProducts() {
-        return fetch(`${API}/catalog.json`)
-            .then(result => result.json())
-            .then(data => {
-                this.productsFetch = [...data]
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
-    #renderPage() {
-        const productContainer = document.querySelector(`${this.catalogSelector}`)
-
-        for (const product of this.productsFetch) {
-            const productObj = new ProductItem(product)
-            this.products.push(productObj)
-            this.filteredItems.push(productObj)
-
-            productContainer.insertAdjacentHTML('beforeend', productObj.render())
-        }
-    }
-    #addToCart(event) {
-        const target = event.target
-
-        if (!target.classList.contains(`${this.addButtonSelector}`)) {
-            return
-        }
-
-        console.clear()
-
-        const id = +target.dataset.id
-
-        let neededItem, changedItem
-        for (const item of this.products) {
-            if (item.id === id) {
-                neededItem = Object.assign({}, item)
-            }
-        }
-        neededItem.count++
-
-        this.cart.cartObjArr.filter(item => {
-            if (item.id === neededItem.id) {
-                changedItem = item
-            }
-        })
-
-        if (changedItem) {
-            changedItem.count += 1
-            this.cart.reRenderCount(changedItem)
-        } else {
-            this.cart.setRawCartItemsArr(neededItem)
-            this.cart.render()
-        }
-    }
-    #filter(event) {
-        const name = document.querySelector('.filter-catalog').value
-        if (name.length <= 2 && name.length > 0) {
-            return
-        }
-        const regExp = new RegExp(name, 'i')
-
-        this.filteredItems = this.products.filter(item => regExp.test(item.title))
-        this.products.forEach(item => {
-            const block = document.querySelector(`.product-item[data-id="${item.id}"]`)
-
-            if (!this.filteredItems.includes(item)) {
-                block.classList.add('hidden')
+            if (isItemExist) {
+                this.cartProducts.forEach(item => {
+                    if (item.id === product.id) {
+                        item.count++
+                    }
+                })
             } else {
-                block.classList.remove('hidden')
+                this.cartProducts.push(product)
             }
-        })
-    }
-}
 
+            this.allPriceCart += product.price
 
-class ItemCart extends ProductItem {
-    constructor(product) {
-        super(product)
-        this.count = product.count
-    }
-    render() {
-        return `
-            <div class="cart-item" data-id="${this.id}"> 
-                <h3>${this.title}</h3>
-                <img class="img-src" src="${this.imgSrc}"  alt="${this.title}">
-                <p>${this.price} рублей</p>
-                <p>Количество: <span class="cart-item__count">${this.count}</span></p>
-                <buttton class="del-btn purp-btn" data-id="${this.id}">Убрать</buttton>
-            </div>
-        `
-    }
-}
-class Cart {
-    constructor(cartBtnSelector = '.cart-btn',
-                cartSelector = '.cart',
-                delSelector = 'del-btn') {
-        this.cartBtnSelector = cartBtnSelector
-        this.cartBtn = '' // button to show and hide the cart
-
-        this.cartSelector = cartSelector
-        this.cart = '' // cart by itself
-
-        this.delSelector = delSelector // to delete an item from the cart
-
-        this.rawCartProducts = [] // render products in the cart by this raw array
-        this.cartObjArr = [] // array with generated objects from ItemCart class
-
-        this.#init()
-    }
-    setRawCartItemsArr(arr) {
-        this.rawCartProducts = [arr]
-    }
-
-
-    #init() {
-        this.#cartInit()
-
-        this.cartBtn.addEventListener('click', () => this.#toggleCart())
-    }
-    #toggleCart() {
-        this.cart.classList.toggle('cart_hidden')
-    } // show/hide the cart
-
-
-    #cartInit() {
-        this.cartBtn = document.querySelector(`${this.cartBtnSelector}`)
-        this.cart = document.querySelector(`${this.cartSelector}`)
-        this.cart.innerHTML = ''
-
-        this.#fetchCart()
-            .then(() => {
-                this.render(this.rawCartProducts)
-
-                this.cart.addEventListener('click', (event) => this.#delFromBasket(event))
+            if (this.cartProducts.length > 0) {
+                this.isCartEmpty = false
+            }
+        },
+        isCartItemExist(id) {
+            let isItemExist = false
+            this.cartProducts.forEach(item => {
+                if (item.id === id) {
+                    isItemExist = true
+                }
             })
-    }
-    #resetCart() {
-        this.cart.innerText = 'Корзина пуста...'
-    } // set an inscription in the empty cart
-    #fetchCart() {
-        return fetch(`cart.json`)
-            .then(result => {
-                return result.json()
+
+            return isItemExist
+        },
+        deleteFromCart(cartProduct) {
+            console.clear()
+            console.log(`удаляем из корзины товар`)
+
+            if (cartProduct.count > 1) {
+                cartProduct.count -= 1
+            } else {
+                this.cartProducts.splice(this.cartProducts.indexOf(cartProduct), 1)
+            }
+
+            this.allPriceCart -= cartProduct.price
+
+            if (this.cartProducts.length === 0) {
+                this.isCartEmpty = true
+            }
+        },
+        changeCartVisibility() {
+            this.cartVisibility = !this.cartVisibility
+        },
+        cartFilter() {
+            const text = this.inputText
+
+            if (text.length <= 2 && text.length > 0) {
+                return
+            }
+            const regExp = new RegExp(text, 'i')
+
+            this.filteredItems = this.products.filter(item => regExp.test(item.title))
+
+            this.products.forEach(item => {
+                const block = document.querySelector(`.product-item[data-id="${item.id}"]`)
+
+                if (!this.filteredItems.includes(item)) {
+                    block.classList.add('hidden')
+                } else {
+                    block.classList.remove('hidden')
+                }
             })
+        }
+    },
+    mounted() {
+        this.fetchProducts(`catalog.json`)
             .then(data => {
-                this.rawCartProducts = [...data]
+                for (const item of data) {
+                    this.products.push(item)
+                }
             })
-            .catch(error => {
-                console.log(error)
+        this.fetchProducts(`${API}/cart.json`)
+            .then(data => {
+                for (const item of data) {
+                    this.cartProducts.push(item)
+                    this.allPriceCart += item.price
+                }
             })
-        // return fetch(`${API}/cart.json`)
-        //     .then(result => {
-        //         return result.json()
-        //     })
-        //     .then(data => {
-        //         this.rawCartProducts = [...data]
-        //         console.log(this.rawCartProducts)
-        //     })
-        //     .catch(error => {
-        //         console.log(error)
-        //     })
-    } // get raw products from the git api
-    render(rawArr = this.rawCartProducts) {
-        // this.cartObjArr = []
-
-        for (const cartItem of rawArr) {
-            const newGeneratedItem = new ItemCart(cartItem)
-            this.cartObjArr.push(newGeneratedItem)
-            this.cart.insertAdjacentHTML('beforeend', newGeneratedItem.render())
-        }
-    } // render the cart
-    #delFromBasket(event) {
-        const target = event.target
-        if (!target.classList.contains(`${this.delSelector}`)) {
-            return
-        }
-        console.clear()
-
-        const id = +target.dataset.id
-
-        let clickedItem
-        for (const item of this.cartObjArr) {
-            if (item.id === id) {
-                clickedItem = item
-            }
-        }
-
-        if (clickedItem.count > 1) {
-            this.cartObjArr[this.cartObjArr.indexOf(clickedItem)].count -= 1
-
-            this.reRenderCount(clickedItem)
-        } else {
-            this.cartObjArr.splice(this.cartObjArr.indexOf(clickedItem), 1)
-            this.cart.querySelector(`.cart-item[data-id="${id}"]`).remove();
-        }
-
-        if (this.cart.childElementCount === 0) {
-            this.#resetCart()
-        }
-    } // delete an item from the cart
-    reRenderCount(clickedItem) {
-        const block = this.cart.querySelector(`.cart-item[data-id="${clickedItem.id}"]`)
-        block.querySelector('.cart-item__count').textContent = clickedItem.count
-    } // rerender solo cart element
-}
-
-const cart = new Cart()
-const catalog = new Catalog()
-console.log('test ssh key')
+    }
+})
